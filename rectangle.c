fa5c7ab95f42f5891a728455f7bb3eea5537e03d
satyavathijonnalagadda <satyavathi2011@gmail.com>
2023-11-06 12:19:18 +0530

    rectagle correct

--- rectangle.c
+++ rectangle.c
@@ -1,18 +1,1 @@
-//c programm with area of rectangle values 
-#include <stdio.h>
-int area(int l,int b);
-int main()
-{
-int l;
-printf("enter the value of l:\n");
-scanf("%d",&l);
-int b;
-printf("enter the value of b:\n");
-scanf("%d",&b);
-printf("the area of rectangle is %d",area(l,b));
-}
-int area(int l,int b)    
-{
-    return (2*(l+b));
 
-}
